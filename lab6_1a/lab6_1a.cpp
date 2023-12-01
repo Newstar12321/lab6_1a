b60cfe8a86c450254cc190aa90ac24cd59c96721
Newstar12321 <tazeev.zhenya@mail.ru>
2023-12-01 22:38:23 +0700

    new

--- lab6_1a/lab6_1a.cpp
+++ lab6_1a/lab6_1a.cpp
@@ -9,49 +9,49 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "ru");
+	setlocale(LC_ALL, "ru");
 
-    size_t m, n;
-    const int MAX_SIZE = 10;
-    int array[MAX_SIZE][MAX_SIZE];
-    int minar[MAX_SIZE];
+	size_t m, n;
+	const int MAX_SIZE = 10;
+	int array[MAX_SIZE][MAX_SIZE];
+	int minar[MAX_SIZE];
 
-    cout << "Введите размер матрицы m*n: ";
-    cin >> m >> n;
+	cout << "Введите размер матрицы m*n: ";
+	cin >> m >> n;
 
-    if ((n < 1) or (m < 1) or (n > MAX_SIZE) or (m > MAX_SIZE))
-    {
-        cerr << "Ошибка: размер массива должен быть не меньше 1 и не больше " << MAX_SIZE;
-        return 1;
-    }
+	if ((n < 1) or (m < 1) or (n > MAX_SIZE) or (m > MAX_SIZE))
+	{
+		cerr << "Ошибка: размер массива должен быть не меньше 1 и не больше " << MAX_SIZE;
+		return 1;
+	}
 
-    cout << "Введите элементы массива:" << endl;
-    for (size_t i = 0; i < m; i++)
-    {
-        for (size_t j = 0; j < n; j++)
-        {
-            cin >> array[i][j];
-        }
-    }
+	cout << "Введите элементы массива:" << endl;
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cin >> array[i][j];
+		}
+	}
 
-    for (size_t i = 0; i < m; i++)
-    {
-        int min = array[i][0];
-        for (size_t j = 0; j < n; j++)
-        {
-            if (array[i][j] < min) min = array[i][j];
-        }
-        minar[i] = min;
-    }
+	for (size_t i = 0; i < m; i++)
+	{
+		int min = array[i][0];
+		for (size_t j = 0; j < n; j++)
+		{
+			if (array[i][j] < min) min = array[i][j];
+		}
+		minar[i] = min;
+	}
 
-    int min = minar[0];
-    for (size_t i = 1; i < m; i++)
-    {
-        if (min < minar[i])
-        {
-            min = minar[i];
-        }
-    }
+	int min = minar[0];
+	for (size_t i = 1; i < m; i++)
+	{
+		if (min < minar[i])
+		{
+			min = minar[i];
+		}
+	}
 
-    cout << min;
+	cout << "Ответ: " << min;
 }
